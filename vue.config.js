56ff94227c8c116dc5bbdf4a036e4b3ad9066813
Lokep <iamlokep@163.com>
2021-10-29 11:43:09 +0800

    feat: 添加cdn

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,27 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+
 module.exports = {
   lintOnSave: false,
-  parallel: false
+  parallel: false,
+  configureWebpack: config => {
+
+    if (process.env.NODE_ENV === "production") {
+      config.externals = { // 不会被打包的库
+       'vue': 'Vue',
+       'vue-router': 'VueRouter',
+      }
+     }
+
+    return {
+      // plugins: [
+      //   new CompressionWebpackPlugin({
+      //     algorithm: 'gzip',
+      //     test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+      //     threshold: 10240,
+      //     minRatio: 0.8
+      //   })
+      // ]
+    }
+  }
 }
